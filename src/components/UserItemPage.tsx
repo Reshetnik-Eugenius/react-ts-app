dc943b26337d91e7bee30276447eecedd0d70ed5
Reshetnik-Eugenius <Reshetnik.Eugenius@gmail.com>
2022-07-08 22:26:40 +0300

    event click button - back

--- src/components/UserItemPage.tsx
+++ src/components/UserItemPage.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { FC, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { IUser } from '../types/types';
 
@@ -9,15 +10,16 @@ interface UserItemPageParams {
 
 const UserItemPage: FC = () => {
     const [user, setUser] = useState<IUser | null>(null);
-    const {id} = useParams<{id: string}>();
+    const params = useParams<{id: string}>();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        fetchUser();
+        fetchUser(); 
     } ,[])
 
     async function fetchUser(){
         try {
-            const response = await axios.get<IUser>('https://jsonplaceholder.typicode.com/users/'+ id);
+            const response = await axios.get<IUser>('https://jsonplaceholder.typicode.com/users/'+ params.id);
             setUser(response.data);
         } catch(e) {
             alert(e);
@@ -25,7 +27,7 @@ const UserItemPage: FC = () => {
     }
   return (
     <div>
-    <button>Back</button>
+    <button onClick={()=>navigate('/users')}>Back</button>
     <h1>Users page: {user?.name}</h1>
     <div>
         {user?.email}
