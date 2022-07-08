8b60e52dc3fe9f6ae6927eb89e66a65664f6abd4
Reshetnik-Eugenius <Reshetnik.Eugenius@gmail.com>
2022-07-08 22:37:16 +0300

    event click user - personal user page

--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { FC, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { IUser } from '../types/types';
 import List from './List';
 import UserItem from './UserItem';
 
 const UserPage: FC = () => {
-    const [users, setUsers] = useState<IUser[]>([]);
+    const [users, setUsers] = useState<IUser[]>([]); 
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetchUsers();
@@ -23,7 +25,12 @@ const UserPage: FC = () => {
   return (
     <List 
         items={users} 
-        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+        renderItem={(user: IUser) => 
+            <UserItem 
+                onClick={(user)=>navigate('/users/'+user.id)} 
+                user={user} 
+                key={user.id} 
+            />}
     />
   )
 }
