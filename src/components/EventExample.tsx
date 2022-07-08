7106a6b26e00d21d41cddca188374881277e0688
Reshetnik-Eugenius <Eugenius@example.com>
2022-07-08 15:25:28 +0300

    hook useRef - not managed input

--- src/components/EventExample.tsx
+++ src/components/EventExample.tsx
@@ -1,15 +1,17 @@
-import React, { ChangeEvent, FC, MouseEvent, DragEvent, useState } from "react";
+import React, { ChangeEvent, FC, MouseEvent, DragEvent, useState, useRef } from "react";
 
 const EventExample: FC = () => {
     const [value, setValue] = useState<string>('');
     const [isDrag, setIsDrag] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value);
     }
 
     const clickHandler = (e: MouseEvent<HTMLButtonElement>) => {
-        console.log(value);
+        // console.log(value);
+        console.log(inputRef.current?.value);
     }
     const dragHandler = (e: DragEvent<HTMLDivElement>) => {
         console.log('DRAG');
@@ -30,7 +32,8 @@ const EventExample: FC = () => {
     
     return (
         <div>
-            <input value={value} onChange={changeHandler} type="text" />
+            <input value={value} onChange={changeHandler} type="text" placeholder="Managed"/>
+            <input ref={inputRef} type="text" placeholder="Not managed"/>
             <button onClick={clickHandler}>Button</button>
 
             <div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
