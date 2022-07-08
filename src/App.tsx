d0244ff0dcbb8599ce452d78ee75ef649b1fd86f
Reshetnik-Eugenius <Eugenius@example.com>
2022-07-08 13:02:26 +0300

    get UserItem from personal data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Card, { CardfVariant } from "./components/Card";
+import UserList from "./components/UserList";
+import { IUser } from "./types/types";
 
 const App = () => {
+    const users: IUser[] = [
+        {id: 1, name: 'Name_1', email: 'email_1@mail.ru', address: {city: 'city_1', street: 'street_1', zipcode: 'zip_1'}},
+        {id: 2, name: 'Name_2', email: 'email_2@mail.ru', address: {city: 'city_2', street: 'street_2', zipcode: 'zip_2'}}
+    ]
+
     return (
         <div>
             <Card 
@@ -12,6 +19,7 @@ const App = () => {
             >
                 <button>Button</button>
             </Card>
+            <UserList users={users}/>
         </div>
     );
 };
